1f6a63513fc8b22185dcf66f305c5b98deea9ba9
Matt Haynes <merkabafox@gmail.com>
2023-10-02 11:56:41 -0400

    added caching strategy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,8 +27,14 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      new GenerateSW(),
+      // new GenerateSW(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'JATE',
         short_name: 'JATE',
         start_url: './',
@@ -38,7 +44,7 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('images', 'icons'),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
